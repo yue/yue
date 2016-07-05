387fc39e8bee070b5a81b58128b3089955ab865a
Cheng Zhao <zcbenz@gmail.com>
2016-07-05 20:31:46 +0900

    Fix building with base on Windows

--- build/toolchain/win/setup_toolchain.py
+++ build/toolchain/win/setup_toolchain.py
@@ -88,7 +88,7 @@ def _LoadToolchainEnv(cpu, sdk_dir):
   # the setup script from the SDK if so. |cpu| should be either
   # 'x86' or 'x64'.
   assert cpu in ('x86', 'x64')
-  if bool(int(os.environ.get('DEPOT_TOOLS_WIN_TOOLCHAIN', 1))) and sdk_dir:
+  if bool(int(os.environ.get('DEPOT_TOOLS_WIN_TOOLCHAIN', 0))) and sdk_dir:
     # Load environment from json file.
     env = os.path.normpath(os.path.join(sdk_dir, 'bin/SetEnv.%s.json' % cpu))
     env = json.load(open(env))['env']
