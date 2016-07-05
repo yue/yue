b0ce91eb350c79dac84add217ff85a93c2ac7262
Cheng Zhao <zcbenz@gmail.com>
2016-07-05 22:10:25 +0900

    Init logging in myapp

--- myapp/main.cc
+++ myapp/main.cc
@@ -2,9 +2,17 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
+#include "base/command_line.h"
 #include "base/logging.h"
 
 int main(int argc, const char *argv[]) {
+  base::CommandLine::Init(argc, argv);
+
+  logging::LoggingSettings settings;
+  settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
+  logging::InitLogging(settings);
+
   LOG(INFO) << "Hello World";
+
   return 0;
 }
