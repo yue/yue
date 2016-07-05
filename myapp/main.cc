8d12bfbb36b6d0722da7edefaebf90cfa370df65
Cheng Zhao <zcbenz@gmail.com>
2016-07-05 17:31:06 +0900

    Add all dependencies of base library

---
+++ myapp/main.cc
@@ -0,0 +1,10 @@
+// Copyright 2016 Cheng Zhao. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#include "base/logging.h"
+
+int main(int argc, const char *argv[]) {
+  LOG(INFO) << "Hello World";
+  return 0;
+}
