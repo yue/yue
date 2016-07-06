c6b04d8d0a220741040a2a4af91bc5e3a386c859
Cheng Zhao <zcbenz@gmail.com>
2016-07-06 20:05:25 +0900

    Add unit tests

---
+++ myapp/myapp_unittests.cc
@@ -0,0 +1,11 @@
+// Copyright 2016 Cheng Zhao. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+class MyAppTest : public testing::Test {
+};
+
+TEST_F(MyAppTest, Dummy) {
+}
