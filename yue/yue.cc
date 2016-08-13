d8820990b418a38a01e369b7c85a670df0fae155
Cheng Zhao <zcbenz@gmail.com>
2016-08-13 20:05:04 +0900

    Make yue load file

--- yue/yue.cc
+++ yue/yue.cc
@@ -2,13 +2,9 @@
 // Use of this source code is governed by the license that can be found in the
 // LICENSE file.
 
-#include "base/bind.h"
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "lua/callback.h"
-
-void PMain() {
-}
 
 int main(int argc, const char *argv[]) {
   base::CommandLine::Init(argc, argv);
@@ -17,9 +13,24 @@ int main(int argc, const char *argv[]) {
   settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
   logging::InitLogging(settings);
 
+  auto* cmd = base::CommandLine::ForCurrentProcess();
+  if (cmd->GetArgs().size() != 1) {
+    LOG(ERROR) << "Script expected";
+    return 1;
+  }
+
   lua::ManagedState state;
   if (!state) {
     LOG(ERROR) << "Failed to create state.";
+    return 1;
+  }
+  luaL_openlibs(state);
+
+  if (luaL_loadfile(state, cmd->GetArgs()[0].c_str()) != LUA_OK ||
+      !lua::PCall(state, nullptr)) {
+    std::string error;
+    lua::Pop(state, &error);
+    LOG(ERROR) << "Error run script: " << error;
     return 1;
   }
 
