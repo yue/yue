5e061c0af29a9a71f9c4eb5bc48db60aab18b910
Cheng Zhao <zcbenz@gmail.com>
2016-08-20 13:45:44 +0900

    Do not use NSTextView for label

--- nativeui/mac/label_mac.mm
+++ nativeui/mac/label_mac.mm
@@ -8,15 +8,39 @@
 
 #include "base/strings/sys_string_conversions.h"
 
+@interface LabelView : NSView {
+ @private
+  NSString* text_;
+}
+@property(nonatomic, copy) NSString* text;
+@end
+
+@implementation LabelView
+
+@synthesize text = text_;
+
+- (void)drawRect:(NSRect)dirtyRect {
+  NSMutableParagraphStyle* paragraphStyle =
+      [[[NSParagraphStyle defaultParagraphStyle] mutableCopy] autorelease];
+  [paragraphStyle setAlignment:NSCenterTextAlignment];
+  NSDictionary* attributes = [NSDictionary
+      dictionaryWithObject:paragraphStyle
+                    forKey:NSParagraphStyleAttributeName];
+  NSAttributedString* text =
+      [[[NSAttributedString alloc] initWithString:text_
+                                       attributes:attributes] autorelease];
+  NSRect frame = NSMakeRect(0, (self.frame.size.height - text.size.height) / 2,
+                            self.frame.size.width, text.size.height);
+  [text_ drawInRect:frame withAttributes:attributes];
+}
+
+@end
+
 namespace nu {
 
 Label::Label(const std::string& text) {
-  NSTextField* label = [[NSTextField alloc] initWithFrame:NSZeroRect];
-  label.drawsBackground = NO;
-  label.bordered = NO;
-  label.editable = NO;
-  label.selectable = NO;
-  label.stringValue = base::SysUTF8ToNSString(text);
+  LabelView* label = [[LabelView alloc] init];
+  label.text = base::SysUTF8ToNSString(text);
   set_view(label);
 }
 
@@ -24,11 +48,11 @@ Label::~Label() {
 }
 
 void Label::SetText(const std::string& text) {
-  static_cast<NSTextField*>(view()).stringValue = base::SysUTF8ToNSString(text);
+  static_cast<LabelView*>(view()).text = base::SysUTF8ToNSString(text);
 }
 
 std::string Label::GetText() {
-  return base::SysNSStringToUTF8(static_cast<NSTextField*>(view()).stringValue);
+  return base::SysNSStringToUTF8(static_cast<LabelView*>(view()).text);
 }
 
 }  // namespace nu
