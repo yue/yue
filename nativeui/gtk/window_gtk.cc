2ae85b306846ede756927abf31397981b621a07a
Cheng Zhao <zcbenz@gmail.com>
2016-09-24 14:04:57 +0900

    Make content bounds aware of frame and menu

--- nativeui/gtk/window_gtk.cc
+++ nativeui/gtk/window_gtk.cc
@@ -19,6 +19,13 @@ gboolean OnClose(GtkWidget *widget, GdkEvent *event, Window* window) {
   return TRUE;
 }
 
+// Force window to allocate size for content view.
+void ForceSizeAllocation(GtkWindow* window, GtkWidget* view) {
+  GdkRectangle rect = { 0, 0 };
+  gtk_window_get_size(window, &rect.width, &rect.height);
+  gtk_widget_size_allocate(view, &rect);
+}
+
 }  // namespace
 
 void Window::PlatformInit(const Options& options) {
@@ -61,10 +68,11 @@ void Window::PlatformSetContentView(Container* container) {
   gtk_container_add(vbox, container->view());
   gtk_box_set_child_packing(GTK_BOX(vbox), container->view(), TRUE, TRUE,
                             0, GTK_PACK_END);
+
+  ForceSizeAllocation(window_, container->view());
 }
 
 void Window::PlatformSetMenuBar(MenuBar* menu_bar) {
-  // TODO(zcbenz): Content bound should take account of menubar.
   GtkContainer* vbox = GTK_CONTAINER(gtk_bin_get_child(GTK_BIN(window_)));
   if (menu_bar_)
     gtk_container_remove(vbox, GTK_WIDGET(menu_bar_->menu()));
@@ -72,26 +80,75 @@ void Window::PlatformSetMenuBar(MenuBar* menu_bar) {
   gtk_container_add(vbox, menu);
   gtk_box_set_child_packing(GTK_BOX(vbox), menu, FALSE, FALSE, 0,
                             GTK_PACK_START);
+
+  ForceSizeAllocation(window_, content_view_->view());
 }
 
 void Window::SetContentBounds(const RectF& bounds) {
-  SetBounds(bounds);
+  RectF window_bounds(bounds);
+  GdkWindow* gdkwindow = gtk_widget_get_window(GTK_WIDGET(window_));
+  if (gdkwindow) {
+    // Get frame size.
+    GdkRectangle rect;
+    gdk_window_get_frame_extents(gdkwindow, &rect);
+    // The position of window without frame, which includes menubar.
+    int x, y;
+    gdk_window_get_position(gdkwindow, &x, &y);
+    window_bounds.Offset(rect.x - x, rect.y - y);
+  }
+  // The position of menubar.
+  GdkRectangle alloc;
+  gtk_widget_get_allocation(content_view_->view(), &alloc);
+  window_bounds.Inset(-alloc.x, -alloc.y, 0, 0);
+
+  gtk_window_resize(window_, window_bounds.width(), window_bounds.height());
+  gtk_window_move(window_, window_bounds.x(), window_bounds.y());
+  ForceSizeAllocation(window_, content_view_->view());
 }
 
 RectF Window::GetContentBounds() const {
-  return GetBounds();
+  GdkWindow* gdkwindow = gtk_widget_get_window(GTK_WIDGET(window_));
+  // The position of window without frame, which includes menubar.
+  int x, y;
+  if (gdkwindow)
+    gdk_window_get_position(gdkwindow, &x, &y);
+  else
+    gtk_window_get_position(window_, &x, &y);
+  // The relative position of content view.
+  GdkRectangle alloc;
+  gtk_widget_get_allocation(content_view_->view(), &alloc);
+  return RectF(x + alloc.x, y + alloc.y, alloc.width, alloc.height);
 }
 
 void Window::SetBounds(const RectF& bounds) {
+  SizeF window_size(bounds.size());
+  GdkWindow* gdkwindow = gtk_widget_get_window(GTK_WIDGET(window_));
+  if (gdkwindow) {
+    // Get frame size.
+    GdkRectangle rect;
+    gdk_window_get_frame_extents(gdkwindow, &rect);
+    // Subtract gdk window size to get frame insets.
+    window_size.Enlarge(-(rect.width - gdk_window_get_width(gdkwindow)),
+                        -(rect.height - gdk_window_get_height(gdkwindow)));
+  }
+
+  gtk_window_resize(window_, window_size.width(), window_size.height());
   gtk_window_move(window_, bounds.x(), bounds.y());
-  gtk_window_resize(window_, bounds.width(), bounds.height());
+  ForceSizeAllocation(window_, content_view_->view());
 }
 
 RectF Window::GetBounds() const {
-  int x, y, width, height;
-  gtk_window_get_position(window_, &x, &y);
-  gtk_window_get_size(window_, &width, &height);
-  return RectF(x, y, width, height);
+  GdkWindow* gdkwindow = gtk_widget_get_window(GTK_WIDGET(window_));
+  if (gdkwindow) {
+    GdkRectangle rect;
+    gdk_window_get_frame_extents(gdkwindow, &rect);
+    return RectF(Rect(rect));
+  } else {
+    int x, y, width, height;
+    gtk_window_get_position(window_, &x, &y);
+    gtk_window_get_size(window_, &width, &height);
+    return RectF(x, y, width, height);
+  }
 }
 
 void Window::SetVisible(bool visible) {
