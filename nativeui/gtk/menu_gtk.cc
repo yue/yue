504426f549e31738ed6c1f43d95770cd21b92211
Cheng Zhao <zcbenz@gmail.com>
2016-09-27 11:55:01 +0800

    Popup should be blocking call

--- nativeui/gtk/menu_gtk.cc
+++ nativeui/gtk/menu_gtk.cc
@@ -4,7 +4,17 @@
 
 #include "nativeui/menu.h"
 
+#include "base/run_loop.h"
+
 namespace nu {
+
+namespace {
+
+void OnMenuHidden(GtkWidget* widget, base::RunLoop* loop) {
+  loop->Quit();
+}
+
+}  // namespace
 
 Menu::Menu() : MenuBase(GTK_MENU_SHELL(gtk_menu_new())) {
 }
@@ -12,6 +22,12 @@ Menu::Menu() : MenuBase(GTK_MENU_SHELL(gtk_menu_new())) {
 void Menu::Popup() {
   gtk_menu_popup(GTK_MENU(menu()), nullptr, nullptr, nullptr, nullptr,
                  0, gtk_get_current_event_time());
+
+  // Block until the menu is hidden.
+  base::RunLoop loop;
+  gint id = g_signal_connect(menu(), "hide", G_CALLBACK(OnMenuHidden), &loop);
+  loop.Run();
+  g_signal_handler_disconnect(menu(), id);
 }
 
 }  // namespace nu
