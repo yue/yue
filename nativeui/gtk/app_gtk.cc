6913cb97589f6fe1672720f184af1a9678f16c94
Cheng Zhao <zcbenz@gmail.com>
2016-09-28 16:01:23 +0800

    Fix building on GTK

---
+++ nativeui/gtk/app_gtk.cc
@@ -0,0 +1,15 @@
+// Copyright 2016 Cheng Zhao. All rights reserved.
+// Use of this source code is governed by the license that can be found in the
+// LICENSE file.
+
+#include "nativeui/app.h"
+
+namespace nu {
+
+void App::PlatformInit() {
+}
+
+void App::PlatformDestroy() {
+}
+
+}  // namespace nu
