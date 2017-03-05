f040b3217ab49ba5c14a8590a90cefe633faaba9
Cheng Zhao <zcbenz@gmail.com>
2017-03-05 11:05:36 +0900

    Follow convention for script names

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -13,7 +13,7 @@ if (process.platform !== 'win32') {
 }
 if (process.platform === 'linux') {
   execSync('python build/linux/sysroot_scripts/install-sysroot.py')
-  spawnSync(process.execPath, ['scripts/update-gold.js'], { stdio: 'inherit' })
+  spawnSync(process.execPath, ['scripts/update_gold.js'], { stdio: 'inherit' })
 }
 
 execSync('git submodule sync --recursive')
