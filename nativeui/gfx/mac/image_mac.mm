0011997272aab1d14be4a1e2d2a97af1ae7733b0
Cheng Zhao <zcbenz@gmail.com>
2017-04-08 21:59:28 +0900

    Fix calculating the scale factor for Image

--- nativeui/gfx/mac/image_mac.mm
+++ nativeui/gfx/mac/image_mac.mm
@@ -21,6 +21,15 @@ Image::Image(const std::string& path)
     float pw = [static_cast<NSImageRep*>([reps objectAtIndex:0]) pixelsWide];
     if (lw > 0 && pw > 0)
       scale_factor_ = pw / lw;
+    // NSImage caculates the DPI from the image automatically, which may not be
+    // the same with the DPI set by the @2x suffix, in this case we need to set
+    // size of NSImage to match the scale factor.
+    float expected = GetScaleFactorFromFilePath(path);
+    if (scale_factor_ != expected) {
+      float ph = [static_cast<NSImageRep*>([reps objectAtIndex:0]) pixelsHigh];
+      [image_ setSize:NSMakeSize(pw / expected, ph / expected)];
+      scale_factor_ = expected;
+    }
   }
 }
 
