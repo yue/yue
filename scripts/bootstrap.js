be0f4ab4a4f0f70a8c7f2b776ef6d2da2540f2b0
Cheng Zhao <zcbenz@gmail.com>
2017-04-29 21:39:37 +0900

    Guess node version in bootstrap

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -4,7 +4,7 @@
 // Use of this source code is governed by the license that can be found in the
 // LICENSE file.
 
-const {execSync} = require('./common')
+const {execSync, spawnSync} = require('./common')
 
 if (process.platform !== 'win32') {
   execSync('python tools/clang/scripts/update.py')
@@ -17,11 +17,20 @@ if (process.platform === 'linux') {
 execSync('git submodule sync --recursive')
 execSync('git submodule update --init --recursive')
 
-gen('out/Debug',
-    'is_component_build=true is_debug=true')
-gen('out/Release',
-    'is_component_build=false is_debug=false is_official_build=true')
+gen('out/Debug', [
+  'is_component_build=true',
+  'is_debug=true',
+  'node_runtime="node"',
+  `node_version="${process.version}"`,
+])
+gen('out/Release', [
+  'is_component_build=false',
+  'is_debug=false',
+  'is_official_build=true',
+  'node_runtime="node"',
+  `node_version="${process.version}"`,
+])
 
 function gen(dir, args) {
-  execSync(`gn gen ${dir} "--args=${args}"`)
+  spawnSync('gn', ['gen', dir, `--args=${args.join(' ')}`])
 }
