62e89959f63e6a059f2c044d431d8e6345807571
Cheng Zhao <zcbenz@gmail.com>
2017-05-13 15:58:26 +0900

    Update to webkit2gtk-4.0

--- nativeui/gtk/browser_gtk.cc
+++ nativeui/gtk/browser_gtk.cc
@@ -11,28 +11,25 @@ namespace nu {
 
 namespace {
 
-inline WebKitWebView* GetWebView(GtkWidget* widget) {
-  return WEBKIT_WEB_VIEW(g_object_get_data(G_OBJECT(widget), "webview"));
+void OnClose(WebKitWebView* widget, Browser* view) {
+  view->on_close.Emit(view);
 }
 
 }  // namespace
 
 Browser::Browser() {
   GtkWidget* webview = webkit_web_view_new();
-  gtk_widget_show(webview);
-  // Add scrollbar.
-  GtkWidget* scroll = gtk_scrolled_window_new(nullptr, nullptr);
-  gtk_container_add(GTK_CONTAINER(scroll), webview);
-  // Make it easier to receive the webview later.
-  g_object_set_data(G_OBJECT(scroll), "webview", webview);
-  TakeOverView(scroll);
+  TakeOverView(webview);
+
+  // Install events.
+  g_signal_connect(webview, "close", G_CALLBACK(OnClose), this);
 }
 
 Browser::~Browser() {
 }
 
 void Browser::LoadURL(const std::string& url) {
-  webkit_web_view_load_uri(GetWebView(GetNative()), url.c_str());
+  webkit_web_view_load_uri(WEBKIT_WEB_VIEW(GetNative()), url.c_str());
 }
 
 }  // namespace nu
