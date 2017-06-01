c9e6f16893db92c9489e268b275451efe816c35c
Cheng Zhao <zcbenz@gmail.com>
2017-06-02 08:40:35 +0900

    Run tests in CI

--- scripts/cibuild.js
+++ scripts/cibuild.js
@@ -7,4 +7,19 @@
 const {execSync} = require('./common')
 
 execSync(`node ./scripts/bootstrap.js --target-cpu=${process.env.TARGET_CPU}`)
-execSync('node ./scripts/build.js out/Release')
+
+const targets = [
+  'node_yue',
+  'lua_yue',
+  'yue',
+]
+execSync(`node ./scripts/build.js out/Release ${targets.join(' ')}`)
+
+const tests = [
+  'nativeui_unittests',
+  'lua_unittests',
+]
+execSync(`node ./scripts/build.js out/Debug ${tests.join(' ')}`)
+
+for (test of tests)
+  execSync(`./out/Debug/${test} --single-process-tests`)
