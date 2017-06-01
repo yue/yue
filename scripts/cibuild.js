b67fdc94be92915f90539cf5f8dac589d13c7090
Cheng Zhao <zcbenz@gmail.com>
2017-06-01 21:34:19 +0900

    Add trivial travis.yml

---
+++ scripts/cibuild.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+// Copyright 2017 Cheng Zhao. All rights reserved.
+// Use of this source code is governed by the license that can be found in the
+// LICENSE file.
+
+const {execSync} = require('./common')
+
+execSync('node ./scripts/bootstrap.js')
+execSync(`node ./scripts/build.js -c out/${process.env.BUILD_TYPE}`)
