120adeebb40883f258ab2422ebbb4986b6927abd
Cheng Zhao <zcbenz@gmail.com>
2017-06-29 09:25:07 +0900

    Do not rely on //base/test for unit tests

--- lua/test/run_all_unittests.cc
+++ lua/test/run_all_unittests.cc
@@ -2,14 +2,9 @@
 // Use of this source code is governed by the license that can be found in the
 // LICENSE file.
 
-#include "base/bind.h"
-#include "base/test/launcher/unit_test_launcher.h"
-#include "base/test/test_suite.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 int main(int argc, char** argv) {
-  base::TestSuite test_suite(argc, argv);
-
-  return base::LaunchUnitTests(
-      argc, argv, base::Bind(&base::TestSuite::Run,
-                             base::Unretained(&test_suite)));
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
