65a15c6b57a70e102aca1387f25be3085930c750
Cheng Zhao <zcbenz@gmail.com>
2017-08-08 17:10:15 +0900

    mac: Disable touchbar when running without bundle

--- nativeui/mac/state_mac.mm
+++ nativeui/mac/state_mac.mm
@@ -13,6 +13,15 @@ void State::PlatformInit() {
   [NSApplication sharedApplication];
   [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
 
+  // Disable TouchBar when the executable is not inside an app bundle, otherwise
+  // Cocoa would just crash.
+  if (![[NSBundle mainBundle] bundleIdentifier]) {
+    NSDictionary* defaults = [NSDictionary
+        dictionaryWithObject:[NSNumber numberWithBool:NO]
+                      forKey:@"NSFunctionBarAPIEnabled"];
+    [[NSUserDefaults standardUserDefaults] registerDefaults:defaults];
+  }
+
   YGConfigSetPointScaleFactor(yoga_config(),
                               [NSScreen mainScreen].backingScaleFactor);
 }
