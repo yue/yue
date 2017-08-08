25b314d5e053352ada67ac13362e07f5d2053812
Cheng Zhao <zcbenz@gmail.com>
2017-08-08 17:39:20 +0900

    mac: TextEdit has to be wrapped in a scroll view

--- nativeui/mac/text_edit_mac.mm
+++ nativeui/mac/text_edit_mac.mm
@@ -4,13 +4,15 @@
 
 #include "nativeui/text_edit.h"
 
+#include "base/mac/scoped_nsobject.h"
 #include "base/strings/sys_string_conversions.h"
 #include "nativeui/gfx/font.h"
 #include "nativeui/mac/nu_private.h"
 #include "nativeui/mac/nu_view.h"
 
-@interface NUTextEdit : NSTextView<NUView> {
+@interface NUTextEdit : NSScrollView<NUView> {
  @private
+  base::scoped_nsobject<NSTextView> textView_;
   nu::NUPrivate private_;
 }
 - (nu::NUPrivate*)nuPrivate;
@@ -21,20 +23,34 @@
 
 @implementation NUTextEdit
 
+- (id)init {
+  if ((self = [super init])) {
+    textView_.reset([[NSTextView alloc] init]);
+    [textView_ setRichText:NO];
+    [textView_ setHorizontallyResizable:YES];
+    [textView_ setVerticallyResizable:YES];
+    [textView_ setAutoresizingMask:(NSViewWidthSizable | NSViewHeightSizable)];
+    [[textView_ textContainer] setContainerSize:NSMakeSize(FLT_MAX, FLT_MAX)];
+    [[textView_ textContainer] setWidthTracksTextView:YES];
+    self.documentView = textView_.get();
+  }
+  return self;
+}
+
 - (nu::NUPrivate*)nuPrivate {
   return &private_;
 }
 
 - (void)setNUFont:(nu::Font*)font {
-  [self setFont:font->GetNative()];
+  [textView_ setFont:font->GetNative()];
 }
 
 - (void)setNUColor:(nu::Color)color {
-  [self setTextColor:color.ToNSColor()];
+  [textView_ setTextColor:color.ToNSColor()];
 }
 
 - (void)setNUBackgroundColor:(nu::Color)color {
-  [self setBackgroundColor:color.ToNSColor()];
+  [textView_ setBackgroundColor:color.ToNSColor()];
 }
 
 @end
@@ -42,15 +58,19 @@
 namespace nu {
 
 TextEdit::TextEdit() {
-  NSTextView* text_view = [[NUTextEdit alloc] init];
-  TakeOverView(text_view);
+  NUTextEdit* edit = [[NUTextEdit alloc] init];
+  [edit setBorderType:NSNoBorder];
+  [edit setHasVerticalScroller:YES];
+  [edit setHasHorizontalScroller:YES];
+  TakeOverView(edit);
 }
 
 TextEdit::~TextEdit() {
 }
 
 std::string TextEdit::GetText() const {
-  auto* text_view = static_cast<NSTextView*>(GetNative());
+  auto* edit = static_cast<NUTextEdit*>(GetNative());
+  auto* text_view = static_cast<NSTextView*>([edit documentView]);
   return base::SysNSStringToUTF8([[text_view textStorage] string]);
 }
 
