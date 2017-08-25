1999acba61568d9bd2da17d3c0f6c8f267d6ad14
Cheng Zhao <zcbenz@gmail.com>
2017-08-25 10:55:30 +0900

    mac: Build with 10.12 SDK

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -27,14 +27,14 @@ import zipfile
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # https://chromium.googlesource.com/chromium/src/+/master/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
-CLANG_REVISION = '305735'
+CLANG_REVISION = '310694'
 
 use_head_revision = 'LLVM_FORCE_HEAD_REVISION' in os.environ
 if use_head_revision:
   CLANG_REVISION = 'HEAD'
 
 # This is incremented when pushing a new build of Clang at the same revision.
-CLANG_SUB_REVISION=1
+CLANG_SUB_REVISION=2
 
 PACKAGE_VERSION = "%s-%s" % (CLANG_REVISION, CLANG_SUB_REVISION)
 
