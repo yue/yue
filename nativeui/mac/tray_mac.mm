d79e37c1a863a7de44b41d96257c8278ee806ac8
Cheng Zhao <zcbenz@gmail.com>
2018-04-08 10:51:20 +0900

    mac: Implement Tray

--- nativeui/mac/tray_mac.mm
+++ nativeui/mac/tray_mac.mm
@@ -4,12 +4,41 @@
 
 #include "nativeui/tray.h"
 
+#import <Cocoa/Cocoa.h>
+
+#include "base/strings/sys_string_conversions.h"
+#include "nativeui/gfx/image.h"
+#include "nativeui/menu.h"
+
 namespace nu {
 
-Tray::Tray(Image* icon) {
+Tray::Tray(Image* icon)
+    : tray_([[[NSStatusBar systemStatusBar]
+                statusItemWithLength:NSSquareStatusItemLength] retain]) {
+  SetImage(icon);
+}
+
+Tray::Tray(const std::string& title)
+    : tray_([[[NSStatusBar systemStatusBar]
+                statusItemWithLength:NSVariableStatusItemLength] retain]) {
+  SetTitle(title);
 }
 
 Tray::~Tray() {
+  [tray_ release];
+}
+
+void Tray::SetTitle(const std::string& title) {
+  [tray_ setTitle:base::SysUTF8ToNSString(title)];
+}
+
+void Tray::SetImage(Image* icon) {
+  [tray_ setImage:icon->GetNative()];
+}
+
+void Tray::SetMenu(Menu* menu) {
+  menu_ = menu;
+  [tray_ setMenu:menu->GetNative()];
 }
 
 }  // namespace nu
