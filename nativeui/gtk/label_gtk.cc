2d96dd31d322dd90d39d7d892cca44a06c85f00e
Cheng Zhao <zcbenz@gmail.com>
2019-01-25 12:43:47 +0900

    gtk: Fix Label not responding to events

--- nativeui/gtk/label_gtk.cc
+++ nativeui/gtk/label_gtk.cc
@@ -11,7 +11,11 @@
 namespace nu {
 
 Label::Label(const std::string& text) {
-  TakeOverView(gtk_label_new(text.c_str()));
+  GtkWidget* event_box = gtk_event_box_new();
+  GtkWidget* label = gtk_label_new(text.c_str());
+  gtk_widget_show(label);
+  gtk_container_add(GTK_CONTAINER(event_box), label);
+  TakeOverView(event_box);
   UpdateDefaultStyle();
 }
 
@@ -19,35 +23,39 @@ Label::~Label() {
 }
 
 void Label::PlatformSetText(const std::string& text) {
-  gtk_label_set_text(GTK_LABEL(GetNative()), text.c_str());
+  auto* label = GTK_LABEL(gtk_bin_get_child(GTK_BIN(GetNative())));
+  gtk_label_set_text(label, text.c_str());
 }
 
 std::string Label::GetText() const {
-  return gtk_label_get_text(GTK_LABEL(GetNative()));
+  auto* label = GTK_LABEL(gtk_bin_get_child(GTK_BIN(GetNative())));
+  return gtk_label_get_text(label);
 }
 
 void Label::SetAlign(TextAlign align) {
+  auto* label = GTK_LABEL(gtk_bin_get_child(GTK_BIN(GetNative())));
   gfloat xalign, yalign;
-  gtk_misc_get_alignment(GTK_MISC(GetNative()), &xalign, &yalign);
+  gtk_misc_get_alignment(GTK_MISC(label), &xalign, &yalign);
   if (align == TextAlign::Start)
     xalign = 0.f;
   else if (align == TextAlign::End)
     xalign = 1.f;
   else
     xalign = 0.5f;
-  gtk_misc_set_alignment(GTK_MISC(GetNative()), xalign, yalign);
+  gtk_misc_set_alignment(GTK_MISC(label), xalign, yalign);
 }
 
 void Label::SetVAlign(TextAlign align) {
+  auto* label = GTK_LABEL(gtk_bin_get_child(GTK_BIN(GetNative())));
   gfloat xalign, yalign;
-  gtk_misc_get_alignment(GTK_MISC(GetNative()), &xalign, &yalign);
+  gtk_misc_get_alignment(GTK_MISC(label), &xalign, &yalign);
   if (align == TextAlign::Start)
     yalign = 0.f;
   else if (align == TextAlign::End)
     yalign = 1.f;
   else
     yalign = 0.5f;
-  gtk_misc_set_alignment(GTK_MISC(GetNative()), xalign, yalign);
+  gtk_misc_set_alignment(GTK_MISC(label), xalign, yalign);
 }
 
 SizeF Label::GetMinimumSize() const {
