f83f3d9b7228f62d5f40d594b47b919add044daf
Cheng Zhao <zcbenz@gmail.com>
2021-11-24 08:31:50 +0900

    gtk: ProtocolJob should be released on main thread

--- nativeui/gtk/nu_protocol_stream.cc
+++ nativeui/gtk/nu_protocol_stream.cc
@@ -4,12 +4,13 @@
 
 #include "nativeui/gtk/nu_protocol_stream.h"
 
+#include "nativeui/message_loop.h"
 #include "nativeui/protocol_job.h"
 
 namespace nu {
 
 struct _NUProtocolStreamPrivate {
-  scoped_refptr<ProtocolJob> protocol_job;
+  ProtocolJob* protocol_job;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE(NUProtocolStream,
@@ -17,9 +18,12 @@ G_DEFINE_TYPE_WITH_PRIVATE(NUProtocolStream,
                            G_TYPE_INPUT_STREAM)
 
 static void nu_protocol_stream_finialize(GObject* stream) {
-  // Call in-place destructor since we don't manage its memory.
   NUProtocolStreamPrivate* priv = NU_PROTOCOL_STREAM(stream)->priv;
-  priv->protocol_job.~scoped_refptr<ProtocolJob>();
+  // Free the ProtocolJob on the main thread.
+  ProtocolJob* protocol_job = priv->protocol_job;
+  MessageLoop::PostTask([protocol_job]() {
+    protocol_job->Release();
+  });
 
   G_OBJECT_CLASS(nu_protocol_stream_parent_class)->finalize(stream);
 }
@@ -52,9 +56,9 @@ static void nu_protocol_stream_init(NUProtocolStream* stream) {
 
 GInputStream* nu_protocol_stream_new(ProtocolJob* protocol_job) {
   void* stream = g_object_new(NU_TYPE_PROTOCOL_STREAM, nullptr);
-  // Do in-place new since memory has already been allocated.
   NUProtocolStreamPrivate* priv = NU_PROTOCOL_STREAM(stream)->priv;
-  new(&priv->protocol_job) scoped_refptr<ProtocolJob>(protocol_job);
+  priv->protocol_job = protocol_job;
+  priv->protocol_job->AddRef();
   return G_INPUT_STREAM(stream);
 }
 
